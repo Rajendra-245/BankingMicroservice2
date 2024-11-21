87c76be3253aab477b4519f5fc8b4bbe0beba4cd
Rajendra-245 <141416952+Rajendra-245@users.noreply.github.com>
2024-11-21 23:17:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,33 @@
+# Define the Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet-example"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  resource_group_name = "rg-new"
+}
+
+# Define the Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "subnet-example"
+  resource_group_name  = "rg-new"
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Define the Network Interface
+resource "azurerm_network_interface" "example" {
+  name                = "nic-example"
+  location            = "East US"
+  resource_group_name = "rg-new"
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Define the Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                = "vm-new"
   resource_group_name = "rg-new"
@@ -14,9 +44,10 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   }
 
   os_disk {
-    caching           = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
+  # Reference the network interface ID
   network_interface_ids = [azurerm_network_interface.example.id]
 }
