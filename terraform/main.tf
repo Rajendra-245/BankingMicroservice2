81da081fc638d445782437ca89fc72bf4e71a32d
Rajendra-245 <141416952+Rajendra-245@users.noreply.github.com>
2024-11-22 09:05:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,13 +43,13 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
  security_rule {
-    name                       = "ALLOW8000"
+    name                       = "ALLOW8001"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_range     = "8000"
+    destination_port_range     = "8001"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
